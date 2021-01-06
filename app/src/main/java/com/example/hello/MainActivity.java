7e7186eef768e4d290091a82272088fb8a0d52bb
DESKTOP-336SF71\HONGs <sdat789@naver.com>
2021-01-06 11:51:57 +0900

    layout study

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.image2);
+        imageView2 = (ImageView)findViewById(R.id.image1);
+
     }
 
     public void onButton1Clicked(View v){
@@ -23,5 +32,22 @@ public class MainActivity extends AppCompatActivity {
     public void onButton2Clicked(View v){
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
         startActivity(intent);
+    }
+
+    public void changeImage(View v){
+        System.out.println("clicked");
+        index+=1;
+        if (index > 1){
+            index = 0;
+        }
+
+        if (index == 0){
+            imageView2.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.INVISIBLE);
+        }
+        else if(index == 1){
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+        }
     }
 }
